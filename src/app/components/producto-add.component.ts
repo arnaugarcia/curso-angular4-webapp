1e7a1b5bd4016166cca1853594458ea7f4f0982a
Arnau García <arnau.garcia.gallego@gmail.com>
2017-07-09 16:38:22 +0200

    Subir imágenes

--- src/app/components/producto-add.component.ts
+++ src/app/components/producto-add.component.ts
@@ -1,10 +1,8 @@
-/**
- * Created by arnau on 8/7/17.
- */
+import { GLOBAL } from './../services/global';
+import { Producto } from './../models/producto';
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import {ProductoService} from "../services/producto.service";
-import {Component} from "@angular/core";
-import {Producto} from "../models/producto";
-import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'producto-add',
@@ -12,37 +10,66 @@ import {ActivatedRoute, Router} from "@angular/router";
   providers: [ProductoService]
 })
 
-export class ProductoAddComponent{
-    public titulo: string;
-    public producto: Producto;
+export class ProductoAddComponent {
+  public titulo: string;
+  public producto: Producto;
+  public filesToUpload;
+  public resultUpload;
 
-    constructor(
-      private _productoService: ProductoService,
-      private _route: ActivatedRoute,
-      private _router: Router
-    ){
-      this.titulo = 'Crear un nuevo producto';
-      this.producto = new Producto('', '', '', '', '');
+  constructor(
+    private _productoService: ProductoService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {
+    this.titulo = 'Crear un nuevo producto';
+    this.producto = new Producto('', '', '', '', '');
+  }
+
+  ngOnInit() {
+    console.log('Producto-add.component.ts cargado!');
+  }
+
+  onSubmit() {
+    console.log(this.producto);
+
+    if (this.filesToUpload.length >= 1) {
+      this._productoService.makeFileRequest(GLOBAL.url + 'upload-file', [], this.filesToUpload).then((result) => {
+        console.log(result);
+        this.resultUpload = result;
+        this.producto.imagen = this.resultUpload.filename;
+        this.saveProducto();
+
+
+      }, (error) => {
+        console.log(error);
+      });
+    } else {
+      this.saveProducto();
     }
 
-    ngOnInit(){
-      console.log('producto-add.component cargado');
-    }
+  }
 
-    onSubmit(){
-      console.log(this.producto);
-      this._productoService.addProducto(this.producto).subscribe(
-          response => {
-              if (response.code == 200){
-                this._router.navigate(['productos']);
-              }else{
-                console.error(response);
-              }
-          },
-          error => {
-              console.error(<any> error);
-          }
-      );
-    }
+  saveProducto() {
+    this._productoService.addProducto(this.producto).subscribe(
+      response => {
+        if (response.code == 200) {
+          this._router.navigate(['/home']);
+        } else {
+          console.log(response);
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+
+
+
+  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+    console.log(this.filesToUpload);
+  }
 
 }
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -1,15 +1,13 @@
-/**
- * Created by arnau on 5/7/17.
- */
-import {Injectable} from "@angular/core";
-import {GLOBAL} from "./global";
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
-import { Http, Response, Headers, RequestOptions } from "@angular/http";
-import {Producto} from "../models/producto";
+import { GLOBAL } from './global';
+import { Producto } from './../models/producto';
+
 @Injectable()
 export class ProductoService{
-  public url: string;
+  public url:string;
 
   constructor(
     public _http: Http
@@ -18,16 +16,40 @@ export class ProductoService{
   }
 
   getProductos(){
-      return this._http.get(this.url + 'productos').map(res => res.json());
+    return this._http.get(this.url+'productos').map(res => res.json());
   }
 
   addProducto(producto: Producto){
     let json = JSON.stringify(producto);
     let params = 'json='+json;
-    let headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
+    let headers = new Headers({'Content-type':'application/x-www-form-urlencoded'});
 
-    return this._http.post(this.url + 'productos', params, {headers: headers})
+    return this._http.post(this.url+'productos', params, {headers: headers})
       .map(res => res.json());
+  }
+
+  makeFileRequest(url: string, params:Array<string>, files:Array<File>){
+    return new Promise((resolve,reject)=>{
+      var formData: any = new FormData();
+      var xhr = new XMLHttpRequest();
+      for(var i = 0; i<files.length; i++){
+        formData.append('uploads[]',files[i],files[i].name);
+      }
+
+      xhr.onreadystatechange = function(){
+        if (xhr.readyState == 4){
+          if (xhr.status == 200){
+            resolve(JSON.parse(xhr.response));
+          } else {
+            reject(JSON.parse(xhr.response));
+          }
+        }
+      };
+
+      xhr.open("POST",url, true);
+      xhr.send(formData);
+
+    });
   }
 
 }
